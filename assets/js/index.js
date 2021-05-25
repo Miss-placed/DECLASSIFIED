ff3bd6db8941732d8644b225553463cccf4af4c2
Odinnh <odin.hullekes@gmail.com>
2021-05-25 20:27:58 +0200

    initial multimap setup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,41 @@
-let mapID = "duga";
-let duga = L.map("duga").setView([1, 1], 3);
-L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    maxZoom: 3,
-    minZoom: 0,
-    tileSize: 512,
-    zoomOffset: -1,
-    noWrap: true
-}).addTo(duga);
-let ruka = L.map("ruka").setView([51.505, -0.09], 1);
+// init map tiles
+var dugaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'MapID', tileSize: 512, zoomOffset: -1 }),
+    rukaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'MapID', tileSize: 512, zoomOffset: -1 });
 
-var greenIcon = L.icon({
-    iconUrl: 'leaf-green.png',
-    shadowUrl: 'leaf-shadow.png',
 
-    iconSize: [38, 95], // size of the icon
-    shadowSize: [50, 64], // size of the shadow
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62], // the same for the shadow
-    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+//init layers
+var duga = new L.LayerGroup();
+var dugaMarkers = new L.LayerGroup();
+
+
+var ruka = new L.LayerGroup();
+var rukaMarkers = new L.LayerGroup();
+
+
+var map = L.map('worldMap', {
+    center: [1, 1],
+    zoom: 3,
+    layers: [duga, ruka],
+    tap: true,
+    tapTolerance: 30
 });
+var baseMaps = {
+    "Duga": duga,
+    "Ruka": ruka,
+};
 
-L.tileLayer(`./maps/ruka/{z}/{y}_{x}.png`, {
-    maxZoom: 3,
-    minZoom: 0,
-    tileSize: (1024, 512),
-    zoomOffset: -1,
-    noWrap: true
-}).addTo(ruka);
 
-L.marker([10, 0], { icon: maxisIcon }).addTo(duga)
+
+L.control.layers(baseMaps).addTo(map);
+// marker loop
+L.marker([10, 0], { icon: maxisIcon }).addTo(dugaMarkers)
     .bindPopup(` <h1>${poi.REDACTED[0].Audio[1].name}</h1> ${poi.REDACTED[0].Audio[1].desc}`);
-L.marker([-10, 0], { icon: omegaIcon }).addTo(duga)
+
+L.marker([-10, 0], { icon: omegaIcon }).addTo(rukaMarkers)
     .bindPopup(` <h1>${poi.REDACTED[0].Audio[1].name}</h1> ${poi.REDACTED[0].Audio[1].desc}`);
-L.marker([0, 10], { icon: darkAetherIcon }).addTo(duga)
-    .bindPopup(` <h1>${poi.REDACTED[0].Audio[1].name}</h1> ${poi.REDACTED[0].Audio[1].desc}`);
-L.marker([0, -10], { icon: requiemIcon }).addTo(duga)
-    .bindPopup(` <h1>${poi.REDACTED[0].Audio[1].name}</h1> ${poi.REDACTED[0].Audio[1].desc}`);
+
+dugaTiles.addTo(duga);
+dugaMarkers.addTo(duga);
+
+rukaTiles.addTo(ruka);
+rukaMarkers.addTo(ruka);
