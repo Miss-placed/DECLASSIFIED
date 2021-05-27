3094e1c1fa853565194b85ee2bc4943a56da1597
Odinnh <odin.hullekes@gmail.com>
2021-05-27 11:53:36 +0200

    better the centering of the map

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,147 +1,81 @@
 // init map tiles
-var dieMaschineTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    id: 'dieMaschine',
-    bounds: [
-        [-512, -512],
-        [512, 512]
-    ],
-    noWrap: true
-})
-var firebaseZTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    id: 'firebaseZ',
-    bounds: [
-        [-512, -512],
-        [512, 512]
-    ],
-    noWrap: true
-})
-var dugaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    id: 'duga',
+var duga = new L.LayerGroup();
+var dugaMarkers = new L.LayerGroup();
+var dugaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'duga', tileSize: 512, zoomOffset: -1 })
 
-    // tileSize: 512,
-    // bounds: [
-    //     [-256, -256],
-    //     [256, 256]
-    // ],
-    noWrap: true
-})
-var rukaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    id: 'ruka',
-    bounds: [
-        [-512, -512],
-        [512, 512]
-    ],
-    noWrap: true
-})
-var golovaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    id: 'golova',
-    bounds: [
-        [-512, -512],
-        [512, 512]
-    ],
-    noWrap: true
-})
-var alpineTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    id: 'alpine',
-    bounds: [
-        [-512, -512],
-        [512, 512]
-    ],
-    noWrap: true
-})
-var sanitoriumTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, {
-    id: 'sanitorium',
-    bounds: [
-        [-512, -512],
-        [512, 512]
-    ],
-    noWrap: true
-});
+var ruka = new L.LayerGroup();
+var rukaMarkers = new L.LayerGroup();
+var rukaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'ruka', tileSize: 512, zoomOffset: -1 })
 
+var alpine = new L.LayerGroup();
+var alpineMarkers = new L.LayerGroup();
+var alpineTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'alpine', tileSize: 512, zoomOffset: -1 })
 
-//init layers
-var dieMaschine = new L.LayerGroup();
-var dieMaschineMarkers = new L.LayerGroup();
-
+var golova = new L.LayerGroup();
+var golovaMarkers = new L.LayerGroup();
+var golovaTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'golova', tileSize: 512, zoomOffset: -1 })
 
 var firebaseZ = new L.LayerGroup();
 var firebaseZMarkers = new L.LayerGroup();
+var firebaseZTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'firebaseZ', tileSize: 512, zoomOffset: -1 })
 
-var duga = new L.LayerGroup();
-var dugaMarkers = new L.LayerGroup();
-var ruka = new L.LayerGroup();
-var rukaMarkers = new L.LayerGroup();
-var golova = new L.LayerGroup();
-var golovaMarkers = new L.LayerGroup();
-var alpine = new L.LayerGroup();
-var alpineMarkers = new L.LayerGroup();
 var sanitorium = new L.LayerGroup();
 var sanitoriumMarkers = new L.LayerGroup();
+var sanitoriumTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'sanitorium', tileSize: 512, zoomOffset: -1 })
 
-dieMaschineTiles.addTo(dieMaschine);
-firebaseZTiles.addTo(firebaseZ);
-
-dugaTiles.addTo(duga);
-rukaTiles.addTo(ruka);
-golovaTiles.addTo(golova);
-alpineTiles.addTo(alpine);
-sanitoriumTiles.addTo(sanitorium);
-
+var dieMaschine = new L.LayerGroup();
+var dieMaschineMarkers = new L.LayerGroup();
+var dieMaschineTiles = L.tileLayer(`./maps/duga/{z}/{x}_{y}.png`, { id: 'dieMaschine', tileSize: 512, zoomOffset: -1 })
 
 var map = L.map('worldMap', {
     crs: L.CRS.Simple,
-    center: [-512, 512],
-    zoom: 1,
-    minZoom: 0,
-    maxZoom: 4,
-    maxBounds: [
-        [-512, -512],
-        [512, 512]
-    ],
+    center: [-(256 / 2), 256 / 2],
+    zoom: 2,
+    maxZoom: 5,
     layers: [
-        dieMaschine,
-        firebaseZ,
         duga,
         ruka,
-        golova,
         alpine,
-        sanitorium
+        golova,
+        firebaseZ,
+        sanitorium,
+        dieMaschine,
     ],
     tap: true,
-    tapTolerance: 30
+    tapTolerance: 30,
+    noWrap: true
 });
 var baseMaps = {
     "Die Maschine": dieMaschine,
-    "Firebase Z": firebaseZ,
+    "firebase Z": firebaseZ,
     "Duga": duga,
     "Ruka": ruka,
-    "Golova": golova,
     "Alpine": alpine,
-    "Sanitorium": sanitorium
+    "golova": golova,
+    "sanitorium": sanitorium,
 };
+L.control.layers(baseMaps, "", { collapsed: false }).addTo(map);
 
-
-
-// marker loop
+// do marker loop here
 L.marker(poi.Requiem[3].Audio[1].loc, { icon: requiemIcon }).addTo(dugaMarkers)
     .bindPopup(` <h1>${poi.Requiem[3].Audio[1].name}</h1> ${poi.Requiem[3].Audio[1].desc}`);
 
 L.marker([-10, 0], { icon: omegaIcon }).addTo(rukaMarkers)
     .bindPopup(` <h1>${poi.REDACTED[0].Audio[1].name}</h1> ${poi.REDACTED[0].Audio[1].desc}`);
 
+dugaTiles.addTo(duga);
+dugaMarkers.addTo(duga);
+rukaTiles.addTo(ruka);
+rukaMarkers.addTo(ruka);
 
-
-dieMaschineMarkers.addTo(dieMaschine);
+alpineTiles.addTo(alpine);
+alpineMarkers.addTo(alpine);
+golovaTiles.addTo(golova);
+golovaMarkers.addTo(golova);
+firebaseZTiles.addTo(firebaseZ);
 firebaseZMarkers.addTo(firebaseZ);
 
-dugaMarkers.addTo(duga);
-rukaMarkers.addTo(ruka);
-golovaMarkers.addTo(golova);
-alpineMarkers.addTo(alpine);
+sanitoriumTiles.addTo(sanitorium);
 sanitoriumMarkers.addTo(sanitorium);
-L.control.layers(baseMaps, "", { collapsed: false }).addTo(map);
-
-// map.fitBounds([
-//     [-512, -512],
-//     [512, 512]
-// ])
+dieMaschineTiles.addTo(dieMaschine);
+dieMaschineMarkers.addTo(dieMaschine);
