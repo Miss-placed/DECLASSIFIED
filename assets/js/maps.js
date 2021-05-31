a5813595abaa2d1c7ffba16af70c6fa72f895887
Nanikos <80385615+Nanik0s@users.noreply.github.com>
2021-05-31 15:25:13 +1000

    Add files via upload

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -25,21 +25,38 @@ var golova = {
     MarkersVisible: true,
     Tiles: L.tileLayer(`./maps/golova/{z}/{x}_{y}.png`, { id: 'golova', tileSize: tileSize, zoomOffset: zoomOffset })
 }
+
+var firebaseZ_spawn = {
+    Layer: new L.LayerGroup(),
+    Markers: new L.LayerGroup(),
+    MarkersVisible: true,
+    Tiles: L.tileLayer(`./maps/firebaseZ_spawn/{z}/{x}_{y}.png`, { id: 'firebaseZ_spawn', tileSize: tileSize, zoomOffset: zoomOffset })
+}
+
 var firebaseZ = {
     Layer: new L.LayerGroup(),
     Markers: new L.LayerGroup(),
     MarkersVisible: true,
     Tiles: L.tileLayer(`./maps/firebaseZ/{z}/{x}_{y}.png`, { id: 'firebaseZ', tileSize: tileSize, zoomOffset: zoomOffset })
 }
+
 var sanatorium = {
     Layer: new L.LayerGroup(),
     Markers: new L.LayerGroup(),
     MarkersVisible: true,
     Tiles: L.tileLayer(`./maps/sanatorium/{z}/{x}_{y}.png`, { id: 'sanatorium', tileSize: tileSize, zoomOffset: zoomOffset })
 }
+
 var dieMaschine = {
     Layer: new L.LayerGroup(),
     Markers: new L.LayerGroup(),
     MarkersVisible: true,
     Tiles: L.tileLayer(`./maps/Die Maschine/{z}/{x}_{y}.png`, { id: 'dieMaschine', tileSize: tileSize, zoomOffset: zoomOffset })
+}
+
+var dieMaschine_underground = {
+    Layer: new L.LayerGroup(),
+    Markers: new L.LayerGroup(),
+    MarkersVisible: true,
+    Tiles: L.tileLayer(`./maps/DieMaschine_underground/{z}/{x}_{y}.png`, { id: 'dieMaschine_underground', tileSize: tileSize, zoomOffset: zoomOffset })
 }
