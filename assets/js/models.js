1a64019e83ba3c8653fa7c058f592f416d0e3d59
Ben Ashley <ben@sol3.me>
2021-07-20 01:44:37 +0100

    Add new constants for new POI V2

--- assets/js/models.js
+++ assets/js/models.js
@@ -13,3 +13,25 @@ const mapStrings = {
     firebaseZ:"firebaseZ", 
     firebaseZ_spawn:"firebaseZ_spawn",
 }
+
+const factions = {
+	requiem: "Requiem",
+	omega: "Omega",
+	maxis: "Maxis",
+	darkAether: "Dark Aether",
+}
+
+const seasons = {
+    "Preseason": 0,
+    "Season 1": 1,
+    "Season 2": 2,
+    "Season 3": 3,
+    "Season 4": 4,
+}
+
+const intelTypes = {
+	AUDIO: "Audio Logs",
+	DOCS: "Documents",
+	RADIO: "Radio Transmissions",
+    ARTIFACT: "Artifacts",
+}
