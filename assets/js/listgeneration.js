b1092b9f41e88604886256da1b57bf2add8f528d
Ben Ashley <ben@sol3.me>
2021-07-20 01:47:18 +0100

    update list gen logic to use new flat POI V2

--- assets/js/listgeneration.js
+++ assets/js/listgeneration.js
@@ -1,48 +1,42 @@
-function generateList() {
-    let pointsOfInterest = poi
+
+
+function generateList(pointsOfInterest) {
     let intelList = document.getElementById("intelList")
-    let lib = {
-        Audio: "Audio Logs",
-        Radio: "Radio Transmisions",
-        Documents: "Documents",
-        Artifacts: "Artifacts",
-    }
-    for (faction in pointsOfInterest) {
-        let factionText = ((faction.toString() == "darkAether") ? "Dark Aether" : faction.toString())
-        let factionElement = createElement("section", [faction, 'faction-list'], `<img class="faction-icon" src="./assets/img/icons/faction-${faction}-icon.png">${factionText}`)
+
+    for (const [factionKey, factionValue] of Object.entries(factions)) {
+        let factionElement = createElement("section", [factionKey, 'faction-list'], `<img class="faction-icon" src="./assets/img/icons/faction-${factionKey}-icon.png">${factionValue}`)
         let seasonList = createElement("div", "season-list", "")
-        for (season in pointsOfInterest[faction]) {
-            let seasonText = ((season == 0) ? "Preseason" : `Season ${season}`)
-            let seasonItems = createElement("div", "season-item", seasonText)
+        for (const [seasonKey, seasonValue] of Object.entries(seasons)) {
+            let seasonItems = createElement("div", "season-item", seasonKey)
             let categoryList = createElement("div", "category-list", "")
-            for (category in pointsOfInterest[faction][season]) {
-                let categoryItems = createElement("div", "category-item", `${lib[category]}`)
+            for (const [categoryKey, categoryValue] of Object.entries(intelTypes)) {
+                let categoryItems = createElement("div", "category-item", `${categoryValue}`)
                 let intelList = createElement("div", "category-list", "")
-                for (intel in pointsOfInterest[faction][season][category]) {
-                    let item = pointsOfInterest[faction][season][category][intel]
-                    let intelItem = createElement("div", "intel-item", `${intel}: ${item.name}`, item.id, item.map)
-                    let intelDesc = createElement("div", "intel-desc", "")
-                    let intelLocation = createElement("p", ["intel-subtitle"], item.map)
-                    let description = createElement("p", ["intel-description"], item.desc)
-                    intelDesc.appendChild(intelLocation)
-                    intelDesc.appendChild(description)
+                for (item of pointsOfInterest) {
+                    if (item.faction == factionValue && item.season == seasonValue && item.intelType == categoryValue) {
+
+                        let intelItem = createElement("div", "intel-item", `${item.name}`, item.id, item.map)
+                        let intelDesc = createElement("div", "intel-desc", "")
+                        let intelLocation = createElement("p", ["intel-subtitle"], item.map)
+                        let description = createElement("p", ["intel-description"], item.desc)
+                        intelDesc.appendChild(intelLocation)
+                        intelDesc.appendChild(description)
 
 
 
-                    if (item.loc[0] != 0 && item.loc[1] != 0) {
-                        let location = createElement("button", "item-location", "Locate Intel")
-                        location.onclick = function() {
-                            switchAndFly(item.loc, item.map)
-                            if (isMobile) toggleAside()
+                        if (item.loc[0] != 0 && item.loc[1] != 0) {
+                            let location = createElement("button", "item-location", "Locate Intel")
+                            location.onclick = goToIntel(item)
+                            intelDesc.appendChild(location)
+                            intelDesc.appendChild(genShareButton(item.id))
                         }
-                        intelDesc.appendChild(location)
-                        intelDesc.appendChild(genShareButton(item.id))
+                        intelItem.appendChild(intelDesc)
+                        intelList.appendChild(intelItem)
                     }
-                    intelItem.appendChild(intelDesc)
-                    intelList.appendChild(intelItem)
                 }
                 categoryItems.appendChild(intelList)
-                if (pointsOfInterest[faction][season][category][1] !== undefined) categoryList.appendChild(categoryItems)
+                categoryList.appendChild(categoryItems)
+                /* if (pointsOfInterest[faction][season][category][1] !== undefined) categoryList.appendChild(categoryItems) Need to find out what this was for*/
             }
             seasonItems.appendChild(categoryList)
             seasonList.appendChild(seasonItems)
@@ -55,6 +49,14 @@ function generateList() {
 
 }
 
+function goToIntel(item) {
+    return function () {
+        switchAndFly(item.loc, item.map)
+        if (isMobile)
+            toggleAside()
+    }
+}
+
 function genShareButton(intelId) {
     let shareBtn = createElement("button", ["share-intel", "fas", "fa-external-link-alt"], "")
     shareBtn.setAttribute("onclick", `copyToClipboard("${window.location.origin}${window.location.pathname}?id=${intelId}", "Link Copied To Clipboard")`)
@@ -62,7 +64,7 @@ function genShareButton(intelId) {
 }
 
 function copyToClipboard(text, notif) {
-    const listener = function(ev) {
+    const listener = function (ev) {
         ev.preventDefault();
         ev.clipboardData.setData('text/plain', text);
     };
@@ -103,7 +105,7 @@ function createElement(type, className, inside = undefined, id, map) {
                 tempElement.setAttribute("data-id", id);
             }
             if (map) tempElement.setAttribute("data-map", map);
-            tempElement.onclick = function() {
+            tempElement.onclick = function () {
                 if (this.nextSibling != undefined) this.nextSibling.classList.toggle("visible")
             }
             tempElement.innerHTML = inside
