314ddfecd263ebd0335c1384ea733f633ec071f7
Odinn H <84709673+Odinnh@users.noreply.github.com>
2021-11-02 01:38:33 +0100

    added all onslaught intel to the map

--- assets/js/models.js
+++ assets/js/models.js
@@ -39,6 +39,7 @@ class UserPrefs {
 
 const mapDetails = {
     allOutbreakMaps: new Item({ id: "outbreak", title: "All Outbreak Maps" }),
+//outbreak
     zoo: new Item({ id: "zoo", title: "Zoo" }),
     ruka: new Item({ id: "ruka", title: "Ruka" }),
     duga: new Item({ id: "duga", title: "Duga" }),
@@ -47,6 +48,7 @@ const mapDetails = {
     sanatorium: new Item({ id: "sanatorium", title: "Sanatorium" }),
     collateral: new Item({ id: "collateral", title: "Collateral" }),
     armada: new Item({ id: "armada", title: "Armada" }),
+//roundbased
     dieMaschine: new Item({ id: "dieMaschine", title: "Die Maschine" }),
     dieMaschineUnderground: new Item({ id: "dieMaschine_underground", title: "Die Maschine" }),
     firebaseZ: new Item({ id: "firebaseZ", title: "Firebase Z" }),
@@ -55,6 +57,29 @@ const mapDetails = {
     mauerDerTotenStreets: new Item({ id: "mauerDerToten_streets", title: "Mauer Der Toten" }),
     forsaken: new Item({ id: "forsaken", title: "Forsaken" }),
     forsakenUnderground: new Item({ id: "forsaken_underground", title: "Forsaken" }),
+//onslaught
+    miami: new Item({id: "miami", title:"Miami"}),
+    satellite: new Item({id: "satellite", title:"Satellite"}),
+    moscow: new Item({id: "moscow", title:"Moscow"}),
+    nuketown: new Item({id: "nuketown", title:"Nuketown"}),
+    thePines: new Item({id: "thePines", title:"The Pines"}),
+    express: new Item({id: "express", title:"Express"}),
+    rush: new Item({id: "rush", title:"Rush"}),
+    echelon: new Item({id: "echelon", title:"Echelon"}),
+    driveIn: new Item({id: "driveIn", title:"Drive-In"}),
+    cartel: new Item({id: "cartel", title:"Cartel"}),
+    crossroads: new Item({id: "crossroads", title:"Crossroads"}),
+    raid: new Item({id: "raid", title:"Raid"}),
+    apocalypse: new Item({id: "apocalypse", title:"Apocalypse"}),
+    yamantau: new Item({id: "yamantau", title:"Yamantau"}),
+    standoff: new Item({id: "standoff", title:"Standoff"}),
+    collateralOn: new Item({id: "collateralOn", title:"Collateral (Onslaught)"}),
+    checkmate: new Item({id: "checkmate", title:"Checkmate"}),
+    garrison: new Item({id: "garrison", title:"Garrison"}),
+    deprogram: new Item({id: "deprogram", title:"Deprogram"}),
+
+
+
 };
 
 function FindMapById(mapId) {
@@ -110,6 +135,7 @@ const intelTypes = {
 
 const seasons = {
     preseason: "Preseason",
+    onslaught: "Onslaught",
     season1: "Season 1",
     season2: "Season 2",
     season3: "Season 3",
@@ -144,6 +170,7 @@ const defaultPOIData = {
     challenge: "Obtained through the Challenge Machine",
     special: "Dropped from Special/Elite kills",
     chests: "Dropped from Special/Elite kills or golden chests",
+    onslaught: "Dropped during the onslaught gamemode,",
     nullLoc: "[0,0]"
 }
 
