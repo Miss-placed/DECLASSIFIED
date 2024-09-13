145a43bc5595d498d1c3e7234af14c9bbbb82f31
sol3uk <ben@sol3.me>
2024-09-13 23:42:55 +0100

    Set contributing cursor as crosshair only when contributing

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -6,62 +6,76 @@ import { NotificationProvider } from '../../contexts/NotificationContext/notific
 import { useUserContext } from '../../contexts/UserContext/userContextProvider';
 import { ApplicationContent } from '../ApplicationContent';
 
-const StyledMapContainer = styled(MapContainer) <{ $isMobile?: boolean }>`
+const StyledMapContainer = styled(MapContainer)`
 	&&& {
 		color: var(--clr-color);
 		width: 100vw;
 		height: 100svh;
 		z-index: var(--z-index-map);
-		cursor: crosshair;
 		display: block;
 		grid-column: 1;
 		float: right;
 		z-index: 0;
 		background-color: var(--svg-background);
+
+		
 	}
 `;
 
+const MapWrapper = styled.div <{ $isContributing?: boolean }>`
+${({ $isContributing }) =>
+		$isContributing &&
+		`
+		#worldMap {
+			cursor: crosshair;
+		}
+		`}
+
+`
+
 const MapProvider = () => {
-	const { isMobile } = useUserContext();
+	const { isMobile, contributionState } = useUserContext();
+	console.log("isContributing MAP: ", contributionState.isContributing)
 
 	return (
-		<StyledMapContainer
-			$isMobile={isMobile}
-			id={'worldMap'}
-			center={[256, 256]}
-			zoom={0.8}
-			scrollWheelZoom={true}
-			crs={L.CRS.Simple}
-			maxBounds={
-				isMobile
-					? [
-						// TODO - figure out the best bounds for mobile and desktop
-						[-256, -256],
-						[768, 768],
-					]
-					: [
-						[-256, -256],
-						[768, 768],
-					]
-			}
-			zoomControl={false}
-			maxZoom={5}
-			minZoom={0.1}
-			tap={true}
-			tapTolerance={30}
-			/* noWrap={true} */
-			doubleClickZoom={false}
-			zoomDelta={0.5}
-			wheelPxPerZoomLevel={80}
-			zoomSnap={0}
-			maxBoundsViscosity={0.2}
-		>
-			<NotificationProvider>
-				<DeclassifiedContextProvider>
-					<ApplicationContent />
-				</DeclassifiedContextProvider>
-			</NotificationProvider>
-		</StyledMapContainer>
+		<MapWrapper $isContributing={contributionState.isContributing} >
+			<StyledMapContainer
+				id={'worldMap'}
+				center={[256, 256]}
+				zoom={0.8}
+				scrollWheelZoom={true}
+				crs={L.CRS.Simple}
+				maxBounds={
+					isMobile
+						? [
+							// TODO - figure out the best bounds for mobile and desktop
+							[-256, -256],
+							[768, 768],
+						]
+						: [
+							[-256, -256],
+							[768, 768],
+						]
+				}
+				zoomControl={false}
+				maxZoom={5}
+				minZoom={0.1}
+				tap={true}
+				tapTolerance={30}
+				/* noWrap={true} */
+				doubleClickZoom={false}
+				zoomDelta={0.5}
+				wheelPxPerZoomLevel={80}
+				zoomSnap={0}
+				maxBoundsViscosity={0.2}
+			>
+				<NotificationProvider>
+					<DeclassifiedContextProvider>
+						<ApplicationContent />
+					</DeclassifiedContextProvider>
+				</NotificationProvider>
+			</StyledMapContainer>
+		</MapWrapper>
 	);
 };
 
